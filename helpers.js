5fbff5eab6ed920adaad06f0ce26f817e2aac692
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2015-11-05 16:12:53 -0500

    Dixie Outlet Mall 0.1  /helpers.js

--- helpers.js
+++ helpers.js
@@ -22,7 +22,7 @@ function init(e){
 	if ($('#instafeed').length > 0) {
         var feed = new Instafeed({
             get: 'user',
-            userId: '1353068341',
+            userId: '1285623815',
             clientId: 'da5cf03899eb49a496424d9a76bafa0d',
             template: '<a class="ig-image" target=_blank href="{{link}}" title="{{caption}}" ><img src="{{image}}" /></a>',
             limit : 12,

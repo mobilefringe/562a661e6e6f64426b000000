9e1627334c7120b96cae696c06d5c4c2368667d4
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2015-11-18 11:57:36 -0500

    Dixie Outlet Mall 0.1  /helpers.js

--- helpers.js
+++ helpers.js
@@ -259,7 +259,7 @@ function init_map(reg){
         cursor:'pointer',
         responsive:true,
         zoomLimit: [0,10],
-        viewBox:[500,500,2500,2500]
+        viewBox:[500,500,1500,1500]
     });
     
     

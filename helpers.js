1218f20ec8af4b9423dd5c60239de2b62e613635
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2015-11-19 17:44:41 -0500

    Dixie Outlet Mall 0.1  /helpers.js

--- helpers.js
+++ helpers.js
@@ -303,17 +303,17 @@ function blog_searcher(){
                 $('#search_results_stores').append(div_stores);
                 $('#search_results_stores').show();
             });
-            
-            var h2_promotions = "<h2 id='open_promotions' class='li_open'>(" +s_promos.length + ") Promotions<i class='pull-right fa fa-chevron-down'></i></h2>";
-            $('#search_results_promotions').append(h2_promotions);
-            $.each(s_promos, function(i, v){
-                var div = "<div class='blog_search_results collapse_open_promotions'>";
-                div = div + "<h4><a href='/promotions/" + v.slug + "'>" + v.name + "</a></h4>";
-                div = div + "</div>";
-                $('#search_results_promotions').append(div);
-                $('#search_results_promotions').show();
-            });
-            
+            if(s_promos > 0){
+                var h2_promotions = "<h2 id='open_promotions' class='li_open'>(" +s_promos.length + ") Promotions<i class='pull-right fa fa-chevron-down'></i></h2>";
+                $('#search_results_promotions').append(h2_promotions);
+                $.each(s_promos, function(i, v){
+                    var div = "<div class='blog_search_results collapse_open_promotions'>";
+                    div = div + "<h4><a href='/promotions/" + v.slug + "'>" + v.name + "</a></h4>";
+                    div = div + "</div>";
+                    $('#search_results_promotions').append(div);
+                    $('#search_results_promotions').show();
+                });
+            }            
             // var h2_events = "<h2 id='open_stores' class='li_open'>(" +s_stores.length + ") Stores<i class='pull-right fa fa-chevron-down'></i></h2>";
             // $('#search_results_stores').append(h2_events);
             // $.each(s_stores, function(i, v){

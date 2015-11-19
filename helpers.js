944edc414d562adab56b15d18bfbc23a668ccf32
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2015-11-19 17:29:40 -0500

    Dixie Outlet Mall 0.1  /helpers.js

--- helpers.js
+++ helpers.js
@@ -294,7 +294,7 @@ function blog_searcher(){
             var s_stores = results.stores;
             var s_events = results.events;
             var s_promos = results.promotions;
-            var h2 = "<h2 id='open_stores' class='li_open'>(" +s_stores.length + ") Stores<i class='pull-right fa fa-chevron-down'></i></h2><div id='collapse_open_stores>";
+            var h2 = "<h2 id='open_stores' class='li_open'>(" +s_stores.length + ") Stores<i class='pull-right fa fa-chevron-down'></i></h2>\n<div id='collapse_open_stores>";
             $('#search_results_stores').append(h2);
             var div = "";
             $.each(s_stores, function(i, v){

69d65ab42bb07b099cec0947e0b6811ec77260bd
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2015-11-19 17:35:48 -0500

    Dixie Outlet Mall 0.1  /helpers.js

--- helpers.js
+++ helpers.js
@@ -306,7 +306,7 @@ function blog_searcher(){
             $('.li_open').click(function(){
                 var collapse = "#collapse_" + $(this).attr('id');
                 var collapse_js = "collapse_" + $(this).attr('id');
-                
+                console.log(collapse)
                 $(collapse).slideDown('fast');
                 
             })

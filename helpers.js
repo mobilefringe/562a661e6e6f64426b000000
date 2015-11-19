bfb71fcabc09d6b8d04d4de1e3d6da838cc4a0f0
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2015-11-19 17:40:46 -0500

    Dixie Outlet Mall 0.1  /helpers.js

--- helpers.js
+++ helpers.js
@@ -294,7 +294,7 @@ function blog_searcher(){
             var s_stores = results.stores;
             var s_events = results.events;
             var s_promos = results.promotions;
-            var h2 = "<h2 id='open_stores' class='li_open'>(" +s_stores.length + ") Stores<i class='pull-right fa fa-chevron-down'></i></h2>";
+            var h2_stores = "<h2 id='open_stores' class='li_open'>(" +s_stores.length + ") Stores<i class='pull-right fa fa-chevron-down'></i></h2>";
             $('#search_results_stores').append(h2);
             $.each(s_stores, function(i, v){
                 var div = "<div class='blog_search_results collapse_open_stores'>";
@@ -303,6 +303,29 @@ function blog_searcher(){
                 $('#search_results_stores').append(div);
                 $('#search_results_stores').show();
             });
+            
+            var h2_promotions = "<h2 id='open_stores' class='li_open'>(" +s_stores.length + ") Stores<i class='pull-right fa fa-chevron-down'></i></h2>";
+            $('#search_results_stores').append(h2);
+            $.each(s_stores, function(i, v){
+                var div = "<div class='blog_search_results collapse_open_stores'>";
+                div = div + "<h4><a href='/stores/" + v.slug + "'>" + v.name + "</a></h4>";
+                div = div + "</div>";
+                $('#search_results_stores').append(div);
+                $('#search_results_stores').show();
+            });
+            
+            var h2_events = "<h2 id='open_stores' class='li_open'>(" +s_stores.length + ") Stores<i class='pull-right fa fa-chevron-down'></i></h2>";
+            $('#search_results_stores').append(h2);
+            $.each(s_stores, function(i, v){
+                var div = "<div class='blog_search_results collapse_open_stores'>";
+                div = div + "<h4><a href='/stores/" + v.slug + "'>" + v.name + "</a></h4>";
+                div = div + "</div>";
+                $('#search_results_stores').append(div);
+                $('#search_results_stores').show();
+            });
+            
+            
+            
             $('.li_open').click(function(){
                 var collapse = ".collapse_" + $(this).attr('id');
                 if($(this).hasClass('open')){

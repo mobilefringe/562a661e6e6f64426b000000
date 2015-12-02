835edd4701353f2a09513492662deb2b8f906f1f
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2015-12-02 13:22:22 -0500

    Dixie Outlet Mall 0.1  /helpers.js

--- helpers.js
+++ helpers.js
@@ -36,18 +36,20 @@ function init(e){
     
     $('#subForm').submit(function(e){
         e.preventDefault();
-        $.getJSON(
-            this.action + "?callback=?",
-            $(this).serialize(),
-            function (data) {
-                if (data.Status === 400) {
-                    alert("An error occured while processing your request. Please try again later.");
-                } else { // 200
-                    $("#subscription_confirmed").fadeIn();
-                    $('#subscription_confirmed').delay(2000).fadeOut();
-                    $('#subForm').trigger('reset');
-                }
-        });
+        if($("#agree_newsletter").is(':checked')){
+            $.getJSON(
+                this.action + "?callback=?",
+                $(this).serialize(),
+                function (data) {
+                    if (data.Status === 400) {
+                        alert("An error occured while processing your request. Please try again later.");
+                    } else { // 200
+                        $("#subscription_confirmed").fadeIn();
+                        $('#subscription_confirmed').delay(2000).fadeOut();
+                        $('#subForm').trigger('reset');
+                    }
+            });
+        }
     });
 }
 

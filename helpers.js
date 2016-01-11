c77a3776f2495e452c48e5b7df37447414d0f355
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-01-11 13:05:46 -0500

    Dixie Outlet Mall 1  /helpers.js

--- helpers.js
+++ helpers.js
@@ -4,9 +4,9 @@ function init(e){
     $('.menu_toggler').click(function(){
         $('#navbar').slideToggle();
         if ($('.menu_toggler').attr('src') == 'http://assets.codecloudapp.com/sites/562a661e6e6f64426b000000/image/png/1445970905000/menu.png'){
-            $(this).attr('src', 'http://assets.codecloudapp.com/sites/562a661e6e6f64426b000000/image/png/1452534608000/menu_close.png')
+            $('.menu_toggler').attr('src', 'http://assets.codecloudapp.com/sites/562a661e6e6f64426b000000/image/png/1452534608000/menu_close.png')
         }else{
-            $(this).attr('src', 'http://assets.codecloudapp.com/sites/562a661e6e6f64426b000000/image/png/1445970905000/menu.png')
+            $('.menu_toggler').attr('src', 'http://assets.codecloudapp.com/sites/562a661e6e6f64426b000000/image/png/1445970905000/menu.png')
         }
     })
     

8f7c056c03dd485122b4012e8b7451b30ac4edeb
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-02-09 12:05:42 -0500

    Dixie Outlet Mall 1.01  /helpers.js

--- helpers.js
+++ helpers.js
@@ -3,7 +3,7 @@ function init(e){
      $.fn.snow({flakeColor: '#EE0000', newOn: 1000});
      $.fn.snow({flakeColor: '#0312d7', newOn: 1000});
      $.fn.snow({flakeColor: '#8a03d7', newOn: 1000});
-     $.fn.snow({flakeColor: '#8a03d7', newOn: 1000});
+     $.fn.snow({flakeColor: '#ecda0c', newOn: 1000});
     $('.menu_toggler').click(function(){
         $('#navbar').slideToggle();
         if ($('.menu_toggler').attr('src') == 'http://assets.codecloudapp.com/sites/562a661e6e6f64426b000000/image/png/1445970905000/menu.png'){

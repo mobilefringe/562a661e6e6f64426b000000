cf018b7aac803675f385b168fdf7e9ae41219d44
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-12-15 14:34:40 -0500

    Dixie Outlet Mall 1.4  /application.js

--- application.js
+++ application.js
@@ -350,7 +350,7 @@ function renderPromotions(container, template, collection){
             val.image_url = "//codecloud.cdn.speedyrails.net/sites/57f66e416e6f6465fe050000/image/jpeg/1446753494000/Dixie_default.jpg";
         }
         
-        if(val.image_url.indexOf('missing.png') > 0){
+        if(val.image_url == null){
             val.image_url  = "//codecloud.cdn.speedyrails.net/sites/57f66e416e6f6465fe050000/image/jpeg/1446753494000/Dixie_default.jpg";;
         }
         

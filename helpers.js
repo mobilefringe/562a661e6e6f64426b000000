0bd2f5d82724d18c40233cb64edd7eb2f08fc665
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-04-10 12:59:21 -0400

    Dixie Outlet Mall 1.3  /helpers.js

--- helpers.js
+++ helpers.js
@@ -6,10 +6,8 @@ function init(e){
         $('#navbar').slideToggle();
         if ($('.menu_toggler').attr('src') == '//codecloud.cdn.speedyrails.net/sites/562a661e6e6f64426b000000/image/png/1445970905000/menu.png'){
             $('.menu_toggler').attr('src', '//codecloud.cdn.speedyrails.net/sites/57f66e416e6f6465fe050000/image/png/1452534608000/menu_close.png')
-            $('document').addClass('no_scroll');
         }else{
             $('.menu_toggler').attr('src', '//codecloud.cdn.speedyrails.net/sites/562a661e6e6f64426b000000/image/png/1445970905000/menu.png')
-            $('document').removeClass('no_scroll');
         }
     })
     
